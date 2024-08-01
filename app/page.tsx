27378479a1324bb9f1414c7e75cce03c5f8d15f7
Youjung Lee <youjung.dev@gmail.com>
2024-08-02 06:18:59 +0900

    Fixed Right Panel UI

--- app/page.tsx
+++ app/page.tsx
@@ -16,21 +16,30 @@ export default function Home() {
         <div>
           <LatestPosts />
         </div>
-        <div className="md:ml-5 flex flex-col md:h-[280vh]">
+
+        <div className=" md:ml-5 flex flex-col  md:h-[280vh]">
           
-          <div className="sticky top-10 bottom-0"><div>
-            <h1 className="font-bold mb-4">인기 카테고리</h1>
-            <TopCatogories />
-          </div>
-            <h1 className="mt-10  font-bold mb-4">인기 포스트</h1>
-            <PopularPosts />
-            <Link href="https://www.coupang.com" target="_blank">
-            <div className="mt-10 w-full h-fit bg-blue-600 dark:bg-transparent">
-              <Image src="/coupang-round.png" alt="쿠팡" width={500} height={500} className="w-full h-40 object-scale-down" />
-            </div></Link>
-          </div>
+            <div className="md:sticky w-full top-10 bottom-0">
+              <div className="flex flex-col w-full">
+                <h1 className="font-bold mb-4">인기 카테고리</h1>
+                <TopCatogories />
+              </div>
+
+
+               <div className="flex flex-col w-full"><h1 className="mt-10  font-bold mb-4">인기 포스트</h1>
+
+               <PopularPosts /></div>
+
+              <Link href="https://www.coupang.com" target="_blank">
+                  <div className="mt-10 w-full h-fit bg-blue-600 dark:bg-transparent">
+                      <Image src="/coupang-round.png" alt="쿠팡" width={500} height={500} className="w-full h-40 object-scale-down" />
+                  </div>
+              </Link>
+
+            </div>
         </div>
       </main>
     </Container>
   );
 }
+  
